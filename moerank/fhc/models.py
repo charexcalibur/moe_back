351ae0fce610b922894a3847060cc07b22b7f658
hayato <charexcalibur@gmail.com>
2020-09-23 11:48:05 +0800

    feat(fhc): add quotations model

--- moerank/fhc/models.py
+++ moerank/fhc/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class TimeAbstract(models.Model):
+    add_time = models.DateTimeField(auto_now_add=True, verbose_name="添加时间")
+    modify_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+
+    class Meta:
+        abstract = True
+
+class Quotations(TimeAbstract):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    content = models.TextField(default='', blank=True, verbose_name='内容')
+    author = models.CharField(max_length=100, default='', verbose_name='作者')
