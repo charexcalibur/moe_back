1c508ae9ce5103281f9613d8c4ee16df36c6366b
hayato <charexcalibur@gmail.com>
2020-09-24 17:07:14 +0800

    feat(fhc/quotation): modify action create and update in QuotationsViewSet

--- moerank/fhc/views/quotations.py
+++ moerank/fhc/views/quotations.py
@@ -3,6 +3,8 @@ from moerank.fhc.models import Quotations
 from moerank.fhc.serializers.quotations import QuotationsSerializer
 from moerank.common.custom import CommonPagination, TreeAPIView
 from rest_framework.filters import SearchFilter, OrderingFilter
+import base64
+from rest_framework.response import Response
 
 class QuotationsViewSet(ModelViewSet):
     queryset = Quotations.objects.all()
@@ -11,3 +13,63 @@ class QuotationsViewSet(ModelViewSet):
     filter_backends = (SearchFilter, OrderingFilter)
     search_fields = ('content','author',)
     ordering_fields = ('add_time',)
+
+    def create(self, request):
+        content = request.POST.get('content', None)
+        if not content:
+            res = {
+                'error_no': '7001',
+                'msg': 'content is required'
+            }
+            return Response(res)
+
+        author = request.POST.get('author', None)
+        if not author:
+            res = {
+                'error_no': '7002',
+                'msg': 'author is required'
+            }
+            return Response(res)
+
+        save_data = {
+            'content': base64.b64encode(content.encode()),
+            'author': base64.b64encode(author.encode())
+        }
+
+        p = Quotations.objects.create(**save_data)
+        
+        res = {
+            'error_no': '7003',
+            'result': QuotationsSerializer(p).data
+        }
+        return Response(res)
+
+    def update(self, request, pk=None):
+        content = request.POST.get('content', None)
+        if not content:
+            res = {
+                'error_no': '7001',
+                'msg': 'content is required'
+            }
+            return Response(res)
+
+        author = request.POST.get('author', None)
+        if not author:
+            res = {
+                'error_no': '7002',
+                'msg': 'author is required'
+            }
+            return Response(res)
+
+        save_data = {
+            'content': base64.b64encode(content.encode()),
+            'author': base64.b64encode(author.encode())
+        }
+
+        p = Quotations.objects.filter(id=pk).update(**save_data)
+
+        res = {
+            'error_no': '7004',
+            'result': p
+        }
+        return Response(res)
