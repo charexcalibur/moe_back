bef817eaa61d38b3604d8adb8d72f17afbf0d0ba
hayato <charexcalibur@gmail.com>
2020-10-28 00:25:54 +0800

    feat(menus): dynamic menus bind by roles, return a menu tree result in response

---
+++ moerank/common/filters/filters.py
@@ -0,0 +1,16 @@
+from rest_framework import filters
+from ..models import Menu
+
+class MenuFilterBackend(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        perms = request.user.roles.values(
+            'permissions__method',
+        ).distinct()
+
+        perms_list = [item['permissions__method'] for item in perms]
+
+        if 'admin' in perms_list:
+            return queryset
+        else:
+            menus_id_list = [item['menus'] for item in request.user.roles.values('menus')]
+            return queryset.filter(id__in=menus_id_list)
--- moerank/common/views/menu.py
+++ moerank/common/views/menu.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from ..serializers.menu import MenuSerializer
 from moerank.common.custom import CommonPagination, TreeAPIView, RbacPermission
 from rest_framework.filters import SearchFilter, OrderingFilter
+from moerank.common.filters.filters import MenuFilterBackend
 
 class MenuViewSet(ModelViewSet, TreeAPIView):
     perms_map = ({'*': 'admin'}, {'*': 'menu_all'}, {'get': 'menu_list'}, {'post': 'menu_create'}, {'put': 'menu_edit'},
@@ -11,36 +12,29 @@ class MenuViewSet(ModelViewSet, TreeAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
     pagination_class = CommonPagination
-    filter_backends = (SearchFilter, OrderingFilter)
+    filter_backends = (MenuFilterBackend, SearchFilter, OrderingFilter)
     search_fields = ('name',)
     ordering_fields = ('sort',)
     permission_classes = (RbacPermission,)
 
     def list(self, request):
-        results = [
-            {
-                'path': '/dashboard',
-                'name': 'dashboard',
-                # 'icon': 'dashboard',
-                'children': [
-                    {
-                        'path': '/dashboard/analysis',
-                        'name': 'analysis'
-                    }
-                ]
-            },
-            {
-                'path': '/list',
-                # 'icon': 'table',
-                'name': 'list',                
-                'children': [
-                    {
-                        'name': 'basic-list',
-                        'path': '/list/basic-list'
-                    }
-                ]
-            }
-        ]
+        queryset = self.filter_queryset(self.queryset)
+        serializer = self.get_serializer(queryset, many=True)
+        tree_dict = {}
+        tree_data = []
+        try:
+            for item in serializer.data:
+                tree_dict[item['id']] = item
+            for i in tree_dict:
+                if tree_dict[i]['pid']:
+                    pid = tree_dict[i]['pid']
+                    parent = tree_dict[pid]
+                    parent.setdefault('children', []).append(tree_dict[i])
+                else:
+                    tree_data.append(tree_dict[i])
+            results = tree_data
+        except KeyError:
+            results = serializer.data
         res = {
             'error_no': '2001',
             'results': results
@@ -48,10 +42,14 @@ class MenuViewSet(ModelViewSet, TreeAPIView):
         return Response(res, status=200)
 
 class MenuTreeView(TreeAPIView):
+    """
+    no need for now
+    """
     queryset = Menu.objects.all()
-    filter_backends = (SearchFilter, OrderingFilter)
+    filter_backends = (MenuFilterBackend, SearchFilter, OrderingFilter)
 
     def list(self, request):
+        queryset = self.filter_queryset(self.queryset)
         res = {
             'error_no': '',
             'results': ''
